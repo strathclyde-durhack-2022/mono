6a512011e25e8f824f9493b563e5f64e92696098
k9mil <kamilzak00@gmail.com>
2022-11-19 17:20:32 +0000

    feat: create page index

---
+++ frontend/src/components/HomeMain.tsx
@@ -0,0 +1,16 @@
+function HomeMain() {
+  return (
+    <div className="flex items-center justify-start m-32 h-1/2">
+        <h1 className="
+          xl:text-5xl
+          lg:text-4xl
+          md:text-3xl
+          sm:text-2xl
+          min-[0px]:text-xl
+          max-w-md z-4 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-900
+          ">blockguess — the game in which you can simulate being a day trader!</h1>
+    </div>
+  )
+}
+
+export default HomeMain
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Background from "../components/Background"
+
+function Home() {
+  return (
+    <div className="w-screen h-screen">
+      <Background />
+    </div>
+  )
+}
+
+export default Home
