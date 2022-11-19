42eb99b236637e6b103f45d10c64b0105ba9fa6c
k9mil <kamilzak00@gmail.com>
2022-11-19 19:48:12 +0000

    fix: add bold

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -5,8 +5,8 @@ const Nav = () => {
         <div className="max-h-1/6 sticky top-0 z-50 flex w-screen flex-row justify-center py-6 px-20 backdrop-blur-sm">
             <div className="flex h-full flex-row">
                 <ul className="flex flex-row items-center justify-center space-x-16 font-normal">
-                    <li><Link to="/" className="cursor-pointer transition duration-500 ease-in-out hover:text-blue-700">Home</Link></li>
-                    <li><Link to="/graph" className="cursor-pointer transition duration-500 ease-in-out hover:text-blue-700">Graph</Link></li>
+                    <li><Link to="/" className="cursor-pointer transition font-bold duration-500 ease-in-out hover:text-blue-700">Home</Link></li>
+                    <li><Link to="/graph" className="cursor-pointer transition font-bold duration-500 ease-in-out hover:text-blue-700">Graph</Link></li>
                 </ul>
             </div>
         </div>
