6aa42ee95982c9f6ea677a560b01f8da47bf3273
Sárándi Ábel <sarandi.abel@gmail.com>
2022-11-20 08:09:10 +0000

    update

--- frontend/src/components/GameChart.tsx
+++ frontend/src/components/GameChart.tsx
@@ -34,10 +34,9 @@ export interface ITickerWithData {
   startingPrice: number;
 }
 
-export const MAX_TICKS = 60;
-export const TICKS_PER_SECOND = 1;
+export const MAX_TICKS = 10;
 
-const DEFAULT_COINS = [
+const DEFAULT_COINS: ITickerWithData[] = [
   { internalName: "btcusdt", label: "BTC", colour: "orange" },
   {
     internalName: "ethusdt",
@@ -45,9 +44,9 @@ const DEFAULT_COINS = [
     colour: "gray",
   },
   {
-    internalName: "xmrusdt",
-    label: "XMR",
-    colour: "red",
+    internalName: "ltcusdt",
+    label: "LTC",
+    colour: "darkblue",
   },
 ].map((c) => ({ ...c, data: [], startingPrice: 0 }));
 
@@ -72,8 +71,7 @@ function GameChart() {
   };
 
   useEffect(() => {
-    if (Math.round(tickerCount) === MAX_TICKS * TICKS_PER_SECOND + 1)
-      setStreaming(false);
+    if (Math.round(tickerCount) === MAX_TICKS + 1) setStreaming(false);
   }, [tickerCount]);
 
   return (
@@ -87,12 +85,18 @@ function GameChart() {
               animations: {
                 y: { type: "number", easing: "linear", duration: 1, from: NaN },
               },
-              scales: { y: { ticks: { callback: (value) => `${value}%` } } },
+              scales: {
+                y: {
+                  ticks: { callback: (value) => `${value} %` },
+                  suggestedMin: -0.01,
+                  suggestedMax: 0.01,
+                },
+              },
             }}
             data={{
-              labels: Array(MAX_TICKS * TICKS_PER_SECOND)
+              labels: Array(MAX_TICKS)
                 .fill(0)
-                .map((_, i) => `${Math.round(i / TICKS_PER_SECOND)}s `),
+                .map((_, i) => `${Math.round(i * 10000) / 10000}s `),
               datasets: data.map((d) => ({
                 data: d.data.map((d) => d.Price),
                 label: d.label,
@@ -138,27 +142,51 @@ function GameChart() {
         </div>
         <div className="flex justify-center items-center h-1/3 flex-col">
           <button onClick={() => setStreaming(!streaming)}>
-            stream is {streaming ? "on" : "off"}
+            {streaming ? "STOP" : "START"}
           </button>
           <button onClick={reset}>reset</button>
           <p>ticker: {Math.round(tickerCount)}</p>
-          <p>
-            {selectedCoin ? (
-              <>Your selected coin is {selectedCoin}</>
-            ) : (
-              <>
-                Please choose which coin would gain the most or lose the least
-                in the next minute!
-              </>
-            )}
-          </p>
-          <Guess
-            coins={data.map((d) => d.label)}
-            onChange={(coin) => setSelectedCoin(coin)}
-          />
-          {getFinalScore()
-            ?.map((d, i) => `${i + 1}. ${d.label}`)
-            ?.join(", ")}
+
+          {!selectedCoin && (
+            <p>
+              Please choose which coin would gain the most or lose the least in
+              the next minute!
+            </p>
+          )}
+          {tickerCount < MAX_TICKS / 2 && (
+            <>
+              <Guess
+                coins={data.map((d) => d.label)}
+                onChange={(coin) => setSelectedCoin(coin)}
+                selected={selectedCoin}
+              />
+            </>
+          )}
+          {tickerCount !== 0 && (
+            <p>
+              Current score:{" "}
+              <span
+                style={{
+                  color: selectedCoin
+                    ? selectedCoin === getFinalScore()[0].label
+                      ? "green"
+                      : "red"
+                    : "inherit",
+                }}
+              >
+                {getFinalScore()
+                  ?.map((d, i) => `${i + 1}. ${d.label}`)
+                  ?.join(", ")}
+              </span>
+            </p>
+          )}
+          {Math.round(tickerCount) === MAX_TICKS + 1 && (
+            <p>
+              {selectedCoin === getFinalScore()[0].label
+                ? "You win!"
+                : "You lose!"}
+            </p>
+          )}
         </div>
       </div>
     </div>
--- frontend/src/components/Guess.tsx
+++ frontend/src/components/Guess.tsx
@@ -3,8 +3,9 @@ import { ICoin } from "./Interface";
 const Guess = (props: {
   coins: ICoin[];
   onChange: (selected: string) => void;
+  selected?: string;
 }) => {
-  return <GuessDropdown ticker={props.coins} onChange={props.onChange} />;
+  return <GuessDropdown ticker={props.coins} onChange={props.onChange} selected={props.selected} />;
 };
 
 export default Guess;
